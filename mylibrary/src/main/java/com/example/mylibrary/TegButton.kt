a44b520f1deb1f62723fc9874dc1be885a500391
Josep Cumplido <josepcumplido@gmail.com>
2024-07-03 21:25:11 +0200

    teg button

---
+++ mylibrary/src/main/java/com/example/mylibrary/TegButton.kt
@@ -0,0 +1,12 @@
+package com.example.mylibrary
+
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TegButton(text: String) {
+    Button(onClick = {  }) {
+        Text(text)
+    }
+}
